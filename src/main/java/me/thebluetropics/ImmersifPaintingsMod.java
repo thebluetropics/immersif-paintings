be0332db435d57e1e30240d3d983568c3102f2dd
thebluetropics <another.redtropics@gmail.com>
2023-09-14 15:51:54 +0700

    add structure chest loot modifiers

--- src/main/java/me/thebluetropics/ImmersifPaintingsMod.java
+++ src/main/java/me/thebluetropics/ImmersifPaintingsMod.java
@@ -27,11 +27,73 @@ public class ImmersifPaintingsMod implements ModInitializer {
 			if (LootTables.DESERT_PYRAMID_CHEST.equals(id)) {
 				LootPool.Builder pool = LootPool.builder()
 					.rolls(ConstantLootNumberProvider.create(1.0f))
+					.bonusRolls(ConstantLootNumberProvider.create(0.0f))
 					.with(ItemEntry.builder(ModBlocks.MOUNT_FUJI.asItem()).weight(5))
 					.with(ItemEntry.builder(ModBlocks.GRASS_AND_SKY.asItem()).weight(15))
 					.with(ItemEntry.builder(ModBlocks.THE_MOYAI_VALLEY.asItem()).weight(15))
-					.with(ItemEntry.builder(Items.AIR).weight(15));
+					.with(ItemEntry.builder(Items.AIR).weight(30));
 				tableBuilder.pool(pool);
+			}
+
+			if (LootTables.ABANDONED_MINESHAFT_CHEST.equals(id)) {
+				LootPool.Builder pool = LootPool.builder()
+					.rolls(ConstantLootNumberProvider.create(1.0f))
+					.bonusRolls(ConstantLootNumberProvider.create(0.0f))
+					.with(ItemEntry.builder(ModBlocks.KINRYUZAN_TEMPLE.asItem()).weight(10))
+					.with(ItemEntry.builder(ModBlocks.REFLECTION.asItem()).weight(10))
+					.with(ItemEntry.builder(Items.AIR).weight(30));
+				tableBuilder.pool(pool);
+			}
+
+			if (LootTables.JUNGLE_TEMPLE_CHEST.equals(id)) {
+				tableBuilder.pool(
+					LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1.0f))
+						.bonusRolls(ConstantLootNumberProvider.create(0.0f))
+						.with(ItemEntry.builder(ModBlocks.ALPHA_JOURNEY.asItem()).weight(15))
+						.with(ItemEntry.builder(Items.AIR).weight(30))
+				);
+			}
+
+			if (LootTables.BURIED_TREASURE_CHEST.equals(id)) {
+				tableBuilder.pool(
+					LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1.0f))
+						.bonusRolls(ConstantLootNumberProvider.create(0.0f))
+						.with(ItemEntry.builder(ModBlocks.OCEANIC_JOURNEY.asItem()).weight(10))
+						.with(ItemEntry.builder(ModBlocks.KINRYUZAN_TEMPLE.asItem()).weight(10))
+						.with(ItemEntry.builder(Items.AIR).weight(30))
+				);
+			}
+
+			if (LootTables.SHIPWRECK_TREASURE_CHEST.equals(id)) {
+				tableBuilder.pool(
+					LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1.0f))
+						.bonusRolls(ConstantLootNumberProvider.create(0.0f))
+						.with(ItemEntry.builder(ModBlocks.COLD_AND_COZY.asItem()).weight(10))
+						.with(ItemEntry.builder(Items.AIR).weight(30))
+				);
+			}
+
+			if (LootTables.SHIPWRECK_MAP_CHEST.equals(id)) {
+				tableBuilder.pool(
+					LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1.0f))
+						.bonusRolls(ConstantLootNumberProvider.create(0.0f))
+						.with(ItemEntry.builder(ModBlocks.SEE_THAT_TREE_OVER_THERE.asItem()).weight(15))
+						.with(ItemEntry.builder(Items.AIR).weight(30))
+				);
+			}
+
+			if (LootTables.SHIPWRECK_SUPPLY_CHEST.equals(id)) {
+				tableBuilder.pool(
+					LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1.0f))
+						.bonusRolls(ConstantLootNumberProvider.create(0.0f))
+						.with(ItemEntry.builder(ModBlocks.UP_TO_THE_SKY.asItem()).weight(15))
+						.with(ItemEntry.builder(Items.AIR).weight(30))
+				);
 			}
 		});
 	}
